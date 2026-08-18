1dea0cdc6518104a24ae1a3d4f7993aab05400c6
Adrian <adrianvalle200@gmailcom>
2026-08-18 12:20:25 -0600

    feat: connect FastAPI with PostgreSQL

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from sqlalchemy import  text
+
+from database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-                return {"message": "Backend funcionando correctamente"}
+                return{
+                                "mensage":"Backend funcionando correctamente"
+                }
+
+@app.get("/test-db")
+def home():
+                with engine.connect() as connection:
+                                connection.execute(text("SELECT 1"))
+                                
+                return{
+                                "message":"PostgreSQL conectado correctamente"
+                }
