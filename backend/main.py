11836d87e31b5a898a1e0d9f4ffa8dc77348b8b4
Adrian <adrianvalle200@gmailcom>
2026-08-18 20:16:21 -0600

    feat: add user registration with password hashing

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,30 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy import text
+from sqlalchemy.orm import Session
 
 import models
-from database import Base, engine
-
+from auth import hash_password
+from database import Base, SessionLocal, engine
+from schemas import UserCreate, UserResponse
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def home():
-                return {
-                "message": "Backend funcionando correctamente"
+    return {
+        "message": "Backend funcionando correctamente"
     }
 
 
@@ -25,3 +36,31 @@ def test_database():
     return {
         "message": "PostgreSQL conectado correctamente"
     }
+
+
+@app.post("/auth/register", response_model=UserResponse)
+def register_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="El correo ya está registrado"
+        )
+
+    new_user = models.User(
+        name=user.name,
+        email=user.email,
+        password_hash=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
