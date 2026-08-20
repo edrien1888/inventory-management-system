f2080a8f4fe0c8818898286d58d741d034858578
Adrian <adrianvalle200@gmailcom>
2026-08-20 14:48:26 -0600

    fix: allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -28,8 +28,9 @@ from schemas import (
 
 app = FastAPI()
 origins = [
-    "http://localhost:4200",
+     "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://inventory-management-frontend-three-rose.vercel.app",
 ]
 
 app.add_middleware(
